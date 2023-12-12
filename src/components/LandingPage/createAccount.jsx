d8489cece07969ba4fb0099beab0db160c08b446
a-mariam <mariiam22222@gmail.com>
2023-12-12 04:33:39 +0100

    first commit

---
+++ src/components/LandingPage/createAccount.jsx
@@ -0,0 +1,16 @@
+import {Page, ReuseP, TopNav, TextDiv, Section, SmallDiv, Button} from "./styles/styles";
+import CashFlow from "../../assets/images/png/Group 1.png"
+
+
+function CreateAccount(){
+
+
+    return(
+       <Page >
+            <img src={CashFlow}/>
+       </Page>
+    )
+}
+
+
+export default CreateAccount
